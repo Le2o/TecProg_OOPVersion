eb16be87339b5a4275f3351049d9dc2be047e0a1
Alexandr <le2o1995@gmail.com>
2019-02-11 12:36:30 +0700

    Лабораторная 2.3

--- TecProg_OOP/Language.cpp
+++ TecProg_OOP/Language.cpp
@@ -25,9 +25,11 @@ Filippov::Language* Filippov::Language::Language_Input(ifstream &fin)
 void Filippov::Language::Input(ifstream &fin)
 {
 	fin >> year_of_development;
+	fin >> reference;
 }
 
 void Filippov::Language::Output(ofstream &fout)
 {
-	fout << "Year of development = " << year_of_development << endl;
+	fout << "Year of development = " << year_of_development
+		<< ", The number of references of this language on the Internet = " << reference << endl;
 }
--- TecProg_OOP/Language.h
+++ TecProg_OOP/Language.h
@@ -17,6 +17,7 @@ namespace Filippov
 		virtual void Output(ofstream &fout);
 	private:
 		unsigned short int year_of_development;
+		int reference;
 	};
 }
 
